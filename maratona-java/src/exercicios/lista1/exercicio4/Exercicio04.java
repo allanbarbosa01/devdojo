49d74bbffcb71f0e9eb45d57d0f2c84cb60763d1
Állan Barbosa <crash171@gmail.com>
2024-08-04 00:35:34 -0300

    Exercicio 4 Check 2

--- maratona-java/src/exercicios/lista1/exercicio4/Exercicio04.java
+++ maratona-java/src/exercicios/lista1/exercicio4/Exercicio04.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 public class Exercicio04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Atribua um valor a A: ");
-        int a = sc.nextInt();
-        int b = a+1;
-        int c = a-1;
-        System.out.println("Seu antecessor é: "+c+" e seu sucessor é: "+b);
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+        int sucessor = numero + 1;
+        int antecessor = numero - 1;
+        System.out.println("Seu antecessor é " + antecessor + " e seu sucessor é " + sucessor);
 
     }
+
 
 
 }
---
+++ maratona-java/src/exercicios/lista1/exercicio5/Exercicio05.java
@@ -0,0 +1,5 @@
+//5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse
+package exercicios.lista1.exercicio5;
+
+public class Exercicio05 {
+}

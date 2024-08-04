2a8f5cb8b7a86c883a7e9aa58ad70d24d1fa359c
Állan Barbosa <crash171@gmail.com>
2024-08-04 00:24:17 -0300

    Exercicio 4 Check

--- maratona-java/src/exercicios/lista1/exercicio4/Exercicio04.java
+++ maratona-java/src/exercicios/lista1/exercicio4/Exercicio04.java
@@ -1,4 +1,18 @@
+//4 - Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.
 package exercicios.lista1.exercicio4;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Atribua um valor a A: ");
+        int a = sc.nextInt();
+        int b = a+1;
+        int c = a-1;
+        System.out.println("Seu antecessor é: "+c+" e seu sucessor é: "+b);
+
+    }
+
+
 }

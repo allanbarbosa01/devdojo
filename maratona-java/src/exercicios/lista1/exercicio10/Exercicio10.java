f9ec5224350b805dd3ae37f5f043758d20965cf9
Állan Barbosa <crash171@gmail.com>
2024-08-05 16:48:45 -0300

    ex 9 e 10 ok

---
+++ maratona-java/src/exercicios/lista1/exercicio10/Exercicio10.java
@@ -0,0 +1,20 @@
+// 10 - Faça um algoritmo que leia três notas obtidas por um aluno, e imprima na tela a média das notas.
+package exercicios.lista1.exercicio10;
+
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insira a primeira nota: ");
+        double nota1 = sc.nextDouble();
+        System.out.print("Insira a segunda nota: ");
+        double nota2 = sc.nextDouble();
+        System.out.print("Insira a terceira nota: ");
+        double nota3 = sc.nextDouble();
+        double media = (nota1 + nota2 + nota3) / 3;
+        System.out.printf("MEDIA: %.2f%n",media);
+
+    }
+
+}
--- maratona-java/src/exercicios/lista1/exercicio9/Exercicio09.java
+++ maratona-java/src/exercicios/lista1/exercicio9/Exercicio09.java
@@ -16,9 +16,33 @@
 // Maior ou igual a 40 | Obesidade grau III (mórbida)
 package exercicios.lista1.exercicio9;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio09 {
-
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insira seu peso (kg): ");
+        double peso = sc.nextDouble();
+        System.out.print("Insira sua altura (m): ");
+        double altura = sc.nextDouble();
+        double imc = peso / (altura * altura);
+        System.out.printf("Seu IMC é: %.2f%n", imc);
+        System.out.println("Sua faixa de IMC é: ");
+        if (imc < 18.5) {
+            System.out.println("Abaixo do peso");
+        } else if (imc >= 18.5 && imc < 25) {
+            System.out.println("Peso ideal, parabéns!!");
+        } else if (imc >= 25 && imc < 30) {
+            System.out.println("Levemente acima do peso");
+        } else if (imc >= 30 && imc < 35) {
+            System.out.println("Obesidade grau I");
+        } else if (imc >= 35 && imc < 40) {
+            System.out.println("Obesidade grau II (severa)");
+        } else  {
+            System.out.println("Obesidade grau III (mórbida)");
+        }
+        sc.close();
+    }
 
 }

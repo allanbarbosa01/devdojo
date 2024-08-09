3e1cdca02fd94c42fbc8ee8ab2911cbe9a9f3db6
Állan Barbosa <crash171@gmail.com>
2024-08-09 03:06:59 -0300

    Melhorado algumas coisas no ex12 e add outro teste exercicio12 mas dessa forma inserindo os dados do produto.

---
+++ maratona-java/src/exercicios/lista1/exercicio13/dominio/Exercicio13.java
@@ -0,0 +1,6 @@
+//13 - Faça algoritmo que leia o nome e a idade de uma pessoa e imprima na tela o nome da pessoa e se ela é maior ou menor de idade.
+
+package exercicios.lista1.exercicio13.dominio;
+
+public class Exercicio13 {
+}
---
+++ maratona-java/src/exercicios/lista1/exercicio14/dominio/Exercicio14.java
@@ -0,0 +1,5 @@
+//14 - Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.
+package exercicios.lista1.exercicio14.dominio;
+
+public class Exercicio14 {
+}
